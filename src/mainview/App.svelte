5d2eb4911b248c15345e222d06df187fdab26a38
David Bos <david@davidnet.net>
2026-03-31 06:29:44 +0000

    test

--- src/mainview/App.svelte
+++ src/mainview/App.svelte
@@ -52,7 +52,7 @@
 
       // Define the target date: March 26, 2026
     // Note: Months are 0-indexed in JavaScript/TypeScript (0 = Jan, 2 = March)
-    const targetDate = new Date(2026, 3, 4);
+    const targetDate = new Date(2026, 5, 5);
     const now = new Date();
 
     if (now > targetDate) {
